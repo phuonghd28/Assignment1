8662df0e0f4c2b7c36565f34f54755bde8964269
DESKTOP-70JFRMR\phuon <phuonghd@newit.co.jp>
2022-03-08 20:56:00 +0700

    Assigment 1

---
+++ Assigment2/Data/MyIdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Assigment2.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assigment2.Data
+{
+    public class MyIdentityDbContext : IdentityDbContext<User>
+    {
+        public MyIdentityDbContext() : base("ConnectionString")
+        {
+
+        }
+    }
+}
